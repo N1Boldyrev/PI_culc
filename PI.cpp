23edfadc40123ed1f6785f7257036a935d7269c7
N1Boldyrev <fraidzilla@gmail.com>
2019-02-10 18:56:44 +0300

    Update PI.cpp

--- PI.cpp
+++ PI.cpp
@@ -9,14 +9,18 @@ void pi_func(int n)
 	double pi, sum, h, x;
 	h = 1.0 / (double)n;
 	sum = 0.0;
+	double time_start = clock();
 	for (int i = 1; i <= n; i++)
 	{
 		x = h * ((double)i - 0.5);
 		sum += (4.0 / (1.0 + x * x));
 	}
+	cout << "time= " << (clock() - time_start) / CLOCKS_PER_SEC;
 	double time_end = clock();
 	pi = h * sum;
-	printf("pi is= %.16f", pi); cout << endl;
+	printf("\tpi is= %.16f", pi);
+	cout << "\tn= " << n;
+	cout << endl;
 }
 
 int main()
@@ -29,5 +33,5 @@ int main()
 	pi_func(300000);
 	pi_func(3000000);
 	pi_func(30000000);
-	return 0;
+	pi_func(300000000);
 }
